66d4d8ff0f3a198e6ea96bc4b04147475c92b36c
Adil Souirite <adil.souirite@woodplc.com>
2021-02-01 12:18:46 +0000

    change structure of the app to be ready for large app

--- Python/application/views/views.py
+++ Python/application/views/views.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request
 
-# create app
-app = Flask(__name__)
+views = Blueprint("views", __name__)
 
-# first view
-@app.route("/", methods=["GET", "POST"])
+
+@views.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         username = request.form["username"]
@@ -12,12 +11,3 @@ def index():
         return render_template("page1.html", username=username, password=password)
     items = ["menu1", "menu2", "menu3", "menu4", "menu5"]
     return render_template("index.html", items=items)
-
-
-@app.route("/page1")
-def page():
-    return render_template("page1.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
